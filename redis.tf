1f544153765e69a5024004a5cd8bb3b41a6362f6
spandanakoppaku <skoppaku@gmail.com>
2023-08-04 19:21:55 -0400

    Auto Push

--- redis.tf
+++ redis.tf
@@ -1,13 +1,14 @@
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "roboshop-${var.ENV}-redis"
-  engine               = "redis"
-  node_type            = "cache.m4.large"
-  num_cache_nodes      = 1
-  parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
-  engine_version       = "6.2"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = [aws_security_group.allows_redis.id]
+  cluster_id            = "roboshop-${var.ENV}-redis"
+  engine                = "redis"
+  node_type             = "cache.m4.large"
+  num_cache_nodes       = 1
+  parameter_group_name  = aws_elasticache_parameter_group.redis_pg.name
+  engine_version        = "6.2"
+  port                  = 6379
+  subnet_group_name     = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids    = [aws_security_group.allows_redis.id]
+  
 }
 
 # Creates parameter group needed for elastic cache
